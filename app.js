c1c8bc76239a74cef327608dc8026f6b15059a9a
Anvesh-999 <anvesh.a9100@gmail.com>
2025-10-23 23:36:34 +0530

    Added signup and login using passport

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const ejsMate= require('ejs-mate');
 const ExpressError = require("./utils/ExpressError.js");
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user.js');
 
-const listings=require('./routes/listing.js');
-const reviews=require('./routes/review.js');
+const listingRouter =require('./routes/listing.js');
+const reviewRouter =require('./routes/review.js');
+const userRouter =require('./routes/user.js');
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
@@ -38,6 +42,13 @@ app.get('/',(req,res)=>{
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
@@ -54,8 +65,9 @@ async function connectDB(){
 
 
 
-app.use('/listings',listings);
-app.use('/listings/:id/reviews',reviews);
+app.use('/listings',listingRouter);
+app.use('/listings/:id/reviews',reviewRouter);
+app.use('/',userRouter);
 
 
 
