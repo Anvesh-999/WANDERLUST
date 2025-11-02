dbd3a0cdb305ca9119e3ae6dd13dc769f75a7d7f
Anvesh-999 <anvesh.a9100@gmail.com>
2025-11-02 13:07:48 +0530

    Added marker in map

--- public/js/map.js
+++ public/js/map.js
@@ -3,6 +3,12 @@ mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
         style: 'mapbox://styles/mapbox/streets-v12', // style URL
-        center: [78.4772,17.4065], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     }); 
+
+const marker = new mapboxgl.Marker({color:"red"})
+        .setLngLat(listing.geometry.coordinates)
+        .setPopup(new mapboxgl.Popup({offset: 25})
+       .setHTML(`<h5>${listing.title}</h5><p>Exact loaction will be provided after booking</p>`))
+        .addTo(map);        
